14a0df3fcd50ac9f1242e3fe7054d7e93a158015
JackyView <jackyview2004@163.com>
2026-01-24 14:30:51 +0800

    feat:服务器配置

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title='reksblog', openapi_url="/api/v1/openapi.json", docs_url="/a
               version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://dev.rekindlers.top"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
