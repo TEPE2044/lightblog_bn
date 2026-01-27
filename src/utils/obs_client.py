440a0fe3a417d2efc7a607c739babadc8e2ee018
JackyView <jackyview2004@163.com>
2026-01-27 14:10:16 +0800

    feat:新增obs_client上传方法

---
+++ src/utils/obs_client.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+from fastapi import UploadFile
+from obs import ObsClient
+from pathlib import Path
+
+from starlette.concurrency import run_in_threadpool
+
+from src.config import settings
+
+
+class OBSUtils:
+    def __init__(self):
+        self.client = ObsClient(
+            access_key_id=settings.keiyue_ak,
+            secret_access_key=settings.keiyue_sk,
+            server=settings.obs_endpoint
+        )
+
+
+myObs = OBSUtils()
+myBucket = "projeck"
+
+
+async def img_upload(rid: int, img: UploadFile) -> str | None:
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    print(img.filename)
+    print("---------")
+
+    img_key = f"RImg/{rid}/{timestamp}_img{Path(img.filename).suffix}"
+    # print('endpoint:', repr(settings.obs_endpoint), type(settings.obs_endpoint))
+    print("---------")
+    print(img_key)
+
+    try:
+        iurl = f"https://{myBucket}.obs.cn-south-1.myhuaweicloud.com/{img_key}"
+        print("---------1")
+        myObs.client.putContent(bucketName=myBucket, objectKey=img_key, content=await img.read())
+        print("---------2")
+        return iurl
+
+    except Exception as e:
+        print(e)
+        try:
+            await run_in_threadpool(
+                lambda: myObs.client.deleteObject(bucketName=myBucket, objectKey=img_key)
+            )
+        except Exception:
+            pass
+        return None
