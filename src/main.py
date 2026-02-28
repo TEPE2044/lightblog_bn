c053c83d044159be93e005a4e94decf824b03925
JackyView <jackyview2004@163.com>
2026-02-28 11:06:26 +0800

    update:update version information

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from src.user.router import userRouter
 from src.music.router import musicRouter
 
 app = FastAPI(title='reksblog', openapi_url="/api/v1/openapi.json", docs_url="/api/v1/docs", redoc_url="/api/v1/redoc",
-              version="0.1.0")
+              version="0.5.0")
 
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
@@ -43,9 +43,9 @@ fastapi_cdn_host.patch_docs(app)
 
 v1 = APIRouter(prefix="/api/v1")
 v1.include_router(userRouter)
-v1.include_router(subscribeRouter)
-v1.include_router(searchRouter)
-v1.include_router(noticeRouter)
+v1.include_router(subscribeRouter, tags=['订阅模块'])
+v1.include_router(searchRouter, tags=['搜索模块'])
+v1.include_router(noticeRouter, tags=['公告模块'])
 v1.include_router(authRouter)
 v1.include_router(blogRouter)
 v1.include_router(musicRouter)
