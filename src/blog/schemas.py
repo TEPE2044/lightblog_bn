0ce57e5f9bee79121afe3d7a9dc8bf3f2c20ced7
JackyView <jackyview2004@163.com>
2026-03-14 20:53:36 +0800

    fix:修复了普通博客无法上传（缺少music_id）的错误

--- src/blog/schemas.py
+++ src/blog/schemas.py
@@ -8,7 +8,7 @@ class BlogData(BaseModel):
     content: str = Field(..., min_length=1)
     cover: List[str] = Field(..., max_length=3)
     tags: List[str] = Field(..., max_length=5)
-    music_id: Optional[int]
+    music_id: Optional[int] = None
 
 
 class CursorPageInput(BaseModel):
