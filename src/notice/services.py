18793e3987e501e0b763ada47fdc69005ed70398
JackyView <jackyview2004@163.com>
2026-03-17 16:17:31 +0800

    update:添加注释

--- src/notice/services.py
+++ src/notice/services.py
@@ -8,8 +8,8 @@ async def post_notice(notice: Notice) -> bool:
     print(notice)
     # 这里需要异步上下文管理器
     async with SessionLocal() as db:
-        # TODO:改写成插入notice表
-
+        # TODO:改写成插入blog表
+        # notice作为一种blog类型
         return False
         # stmt = select(Blog).where(Blog.id == 14)
         # res = (await db.execute(stmt)).mappings().first()
