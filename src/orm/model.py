667706ad52d974040cbfb9605518218db039c402
JackyView <jackyview2004@163.com>
2026-03-27 10:23:36 +0800

    update:向Music表中补充update_at字段

--- src/orm/model.py
+++ src/orm/model.py
@@ -379,6 +379,13 @@ class Music(Base):
         nullable=False,
         comment="创建时间"
     )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+        comment="更新时间"
+    )
     state: Mapped[BlogStateEnum] = mapped_column(
         Enum(BlogStateEnum, native_enum=False),
         default=BlogStateEnum.publish,
